64e915546a7fd16168a65330be4ccbf0b4f0fab2
Maulana <maulana_return@yahoo.com>
2020-09-10 12:45:51 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,32 @@
 package com.company;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("test");
+
+        Scanner sc = new Scanner(System.in);
+        int inp = sc.nextInt();
+        for(int i=1; i<=inp ; i++)
+        {
+            for(int j=1;j<=inp; j++){
+                if((i+j)>inp){
+                    if(j==inp)
+                    {
+                        System.out.print("#");
+                    }
+                    else {
+                        System.out.print("# ");
+                    }
+                }else
+                {
+                    System.out.print("  ");
+                }
+            }
+                System.out.println();
+        }
+
     }
 }
